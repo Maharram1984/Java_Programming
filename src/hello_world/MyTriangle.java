12864055e03743da5668226ad72f5977780f8255
mrefi <Mrefiyev84@gmail.com>
2022-02-03 13:20:05 -0500

    Day3

---
+++ src/hello_world/MyTriangle.java
@@ -0,0 +1,13 @@
+package hello_world;
+
+public class MyTriangle {
+
+    public static void main(String[] args){
+        System.out.println(" I live in Virginia\n My hobby is soccer\n Phone number is (202)256-4411");
+        System.out.println("  * \n ***\n*****");
+        System.out.println(5);
+
+    }
+
+
+}
