d7cbd1e9a77e4bb4eb5ee92fd6a3543ba7b29fc1
mrefi <Mrefiyev84@gmail.com>
2022-02-02 20:04:08 -0500

    Day2

---
+++ src/hello_world/Escape.java
@@ -0,0 +1,16 @@
+package hello_world;
+
+public class Escape {
+
+    public static void main(String[] args){
+        System.out.println("\tGreat!");
+        System.out.println("Hello\nWorld");
+        System.out.println("Java\\Programming");
+        System.out.println("\"Hala Madrid\"");
+        System.out.println("user/mr/ccd");
+        System.out.println("user\\mr\\ccd");
+
+    }
+
+}
+
